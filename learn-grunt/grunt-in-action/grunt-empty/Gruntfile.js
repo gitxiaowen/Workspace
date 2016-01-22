9bf58bdd701107607f6700849a350841e41318d5
githubxiaowen <githubxiaowen@gmail.com>
2016-01-22 13:53:45 +0800

    modify gruntfile.js

--- learn-grunt/grunt-in-action/grunt-empty/Gruntfile.js
+++ learn-grunt/grunt-in-action/grunt-empty/Gruntfile.js
@@ -1,2 +1,16 @@
 'use strict'
 
+module.exports = function  (grunt) {
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+
+	var config=
+	{
+		app: 'app',
+		dist: 'dist'
+	}
+
+	grunt.initConfig({
+
+	});
+}
