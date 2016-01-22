87cad4cd7c4ff46b0814765dc3591dae4d97877d
githubxiaowen <githubxiaowen@gmail.com>
2016-01-22 13:39:50 +0800

    configure the workspace

---
+++ learn-grunt/grunt-in-action/grunt-by-yo/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gruntByYoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gruntByYoApp
+ */
+angular.module('gruntByYoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ learn-grunt/grunt-in-action/grunt-by-yo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gruntByYoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gruntByYoApp
+ */
+angular.module('gruntByYoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
